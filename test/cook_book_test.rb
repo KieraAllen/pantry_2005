ee622ce5c524b83ee510e41dfda1e81bfb2284f0
Kiera Allen <kieraaallen@gmail.com>
2020-06-03 11:15:07 -0600

    Test add_recipes method and recipes attr_reader

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,6 +7,7 @@ require './lib/cook_book'
 class CookBookTest < Minitest::Test
 
   def setup
+    @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
   end
@@ -14,15 +15,22 @@ class CookBookTest < Minitest::Test
   def test_it_exists
     assert_instance_of CookBook, @cookbook
   end
+
+  def test_it_can_add_recipes
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
 end
 
 
 
 
 
-# pry(main)> cookbook.add_recipe(recipe1)
+# pry(main)>
 #
-# pry(main)> cookbook.add_recipe(recipe2)
+# pry(main)>
 #
-# pry(main)> cookbook.recipes
+# pry(main)>
 # # => [#<Recipe:0x00007faae69c9698...>, #<Recipe:0x00007faae692a110...>]
