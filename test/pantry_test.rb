c674943f2beca5048ee1fd5895be7c7eaf1cc35e
Kiera Allen <kieraaallen@gmail.com>
2020-06-03 10:28:21 -0600

    Rewrite restock test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -26,9 +26,11 @@ class IngredientTest < Minitest::Test
   def test_it_can_restock_and_check_stock
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
-    @pantry.restock(@ingredient2, 7)
 
     assert_equal 15, @pantry.stock_check(@ingredient1)
+
+    @pantry.restock(@ingredient2, 7)
+
     assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 end
