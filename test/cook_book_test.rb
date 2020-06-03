c847fbe51d95f717f4a3373b0dc496410de226de
Kiera Allen <kieraaallen@gmail.com>
2020-06-03 11:46:00 -0600

    Test highest_calorie_meal method

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -10,26 +10,16 @@ class CookBookTest < Minitest::Test
   def setup
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+
     @cookbook = CookBook.new
+
     @pantry = Pantry.new
+
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
     @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
-  end
 
-  def test_it_exists
-    assert_instance_of CookBook, @cookbook
-  end
-
-  def test_it_can_add_recipes
-    @cookbook.add_recipe(@recipe1)
-    @cookbook.add_recipe(@recipe2)
-
-    assert_equal [@recipe1, @recipe2], @cookbook.recipes
-  end
-
-  def test_it_can_list_ingredients
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
 
@@ -39,15 +29,26 @@ class CookBookTest < Minitest::Test
     @recipe2.add_ingredient(@ingredient1, 2)
     @recipe2.add_ingredient(@ingredient3, 4)
     @recipe2.add_ingredient(@ingredient4, 1)
-    
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_it_can_add_recipes
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_list_ingredients
     assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], @cookbook.ingredients
+  end
+
+  def test_it_can_find_highest_calorie_meal
+    assert_equal @recipe2, @cookbook.highest_calorie_meal
   end
 end
 
 
-# pry(main)> cookbook.highest_calorie_meal
-# # => #<Recipe:0x00007faae692a110...>
-#
 # pry(main)> pantry.restock(ingredient1, 5)
 #
 # pry(main)> pantry.restock(ingredient1, 10)
