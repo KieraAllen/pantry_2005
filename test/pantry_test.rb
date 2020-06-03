efb3f7abaf76e1c9489150454cca3e2e6e7af4cf
Kiera Allen <kieraaallen@gmail.com>
2020-06-03 10:22:37 -0600

    Create pantry_test file and test for existence and default of no stock

---
+++ test/pantry_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class IngredientTest < Minitest::Test
+
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_starts_with_no_stock
+    assert_equal ({}), @pantry.stock
+  end
+end
+
+
+# pry(main)> pantry.stock_check(ingredient1)
+# # => 0
+#
+# pry(main)> pantry.restock(ingredient1, 5)
+#
+# pry(main)> pantry.restock(ingredient1, 10)
+#
+# pry(main)> pantry.stock_check(ingredient1)
+# # => 15
+#
+# pry(main)> pantry.restock(ingredient2, 7)
+#
+# pry(main)> pantry.stock_check(ingredient2)
+# # => 7
